e05d38d515699534613cd180b94bdb8ec4177196
DomDomo <g.domis02@gmail.com>
2021-08-29 22:50:06 +0300

    Add routing

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Link
+        prefetch={true}
+        href={{ pathname: "/[username]", query: { username: "jeffd234" } }}
+      >
+        <a>Jeff's profile'</a>
+      </Link>
+    </div>
+  );
 }
